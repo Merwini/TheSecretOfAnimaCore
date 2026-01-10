a12af229ee9a918c39f1198dd28a6d13d9358ca9
Merwini <86023141+Merwini@users.noreply.github.com>
2026-01-10 14:48:43 -0500

    Hediff_AnimaTreeLink empty(ish) class

---
+++ Source/TheSecretOfAnimaCore/Hediffs/Hediff_AnimaTreeLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace nuff.tsoa.core
+{
+    public class Hediff_AnimaTreeLink : Hediff
+    {
+        private const int CheckInterval = 120;
+    }
+}
