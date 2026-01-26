7f5a90f7263e4e5cacaa06421987e07588a49d18
Merwini <86023141+Merwini@users.noreply.github.com>
2026-01-25 19:02:42 -0500

    Reapply "CompSpecialMeditationFocus, new Harmony patch to interact with it, and a cache to improve performance"
    This reverts commit e14ada2c63d4ea8d925c90e49b5394ac52bd9877.

---
+++ Source/TheSecretOfAnimaCore/Comps/CompProperties_SpecialMeditationFocus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace tsoa.core
+{
+    public class CompProperties_SpecialMeditationFocus : CompProperties
+    {
+        public float meditationTickProgress = 6.666667E-05f; // vanilla default
+        public MeditationFocusDef requiredFocusType;
+
+        public CompProperties_SpecialMeditationFocus()
+        {
+            this.compClass = typeof(CompSpecialMeditationFocus);
+        }
+    }
+}
