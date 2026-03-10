59608cb04f1797d67ab621b8bc2585a1dfb91ef1
Nuff <me.megalomatt@gmail.com>
2026-03-10 08:22:40 -0400

    Silent grove wip 3

---
+++ Source/TheSecretOfAnimaCore/MapGen/GenStep_HiddenEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace tsoa.core;
+
+public class GenStep_HiddenEntities : GenStep
+{
+    public override int SeedPart => 186892493;
+
+    public override void Generate(Map map, GenStepParams parms)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Source/TheSecretOfAnimaCore/MapGen/SitePartWorker_HiddenEntities.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace tsoa.core;
+
+public class SitePartWorker_HiddenEntities : SitePartWorker
+{
+    public override bool IsAvailable()
+    {
+        if (base.IsAvailable())
+        {
+            return ModsConfig.AnomalyActive && Faction.OfEntities != null;
+        }
+        return false;
+    }
+
+    public override string GetArrivedLetterPart(Map map, out LetterDef preferredLetterDef, out LookTargets lookTargets)
+    {
+        string arrivedLetterPart = base.GetArrivedLetterPart(map, out preferredLetterDef, out lookTargets);
+        lookTargets = new LookTargets(map.Parent);
+        return arrivedLetterPart;
+    }
+
+    public override SitePartParams GenerateDefaultParams(float myThreatPoints, PlanetTile tile, Faction faction)
+    {
+        SitePartParams sitePartParams = base.GenerateDefaultParams(myThreatPoints, tile, faction);
+        sitePartParams.threatPoints = Mathf.Max(sitePartParams.threatPoints, FactionDefOf.Entities.MinPointsToGeneratePawnGroup(PawnGroupKindDefOf.Combat));
+        return sitePartParams;
+    }
+}
