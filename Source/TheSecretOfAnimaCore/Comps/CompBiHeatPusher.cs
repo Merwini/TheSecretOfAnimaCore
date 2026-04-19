c19b752049d746455b303b65de0d545632be2370
Merwini <86023141+Merwini@users.noreply.github.com>
2026-04-19 10:10:38 -0400

    CompBiHeatPusher WIP

---
+++ Source/TheSecretOfAnimaCore/Comps/CompBiHeatPusher.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace tsoa.core;
+
+public class CompBiHeatPusher : ThingComp
+{
+    private const float TicksPerSecond = 60f;
+    private const float CloseEnough = 0.01f;
+
+    public CompProperties_BiHeatPusher Props => (CompProperties_BiHeatPusher)props;
+
+    public bool ShouldPushHeatNow(out float temperature)
+    {
+        if (!parent.SpawnedOrAnyParentSpawned || !parent.IsHashIntervalTick(60))
+        {
+            temperature = 0;
+            return false;
+        }
+        CompProperties_BiHeatPusher compProperties_HeatPusher = Props;
+        Room room = parent.GetRoom();
+        if (room == null || (room.UsesOutdoorTemperature && !Props.worksOutdoors))
+        {
+            temperature = 0;
+            return false;
+        }
+
+        temperature = room.Temperature;
+        if (Props.targetTemperatureRange.Includes(temperature))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public override void CompTick()
+    {
+        base.CompTick();
+
+        if (!ShouldPushHeatNow(out float currentTemp))
+        {
+            return;
+        }
+        Map map = parent.Map;
+        float targetTemp = GetDesiredTargetTemperature(currentTemp);
+
+        float diff = targetTemp - currentTemp;
+        if (Mathf.Abs(diff) < CloseEnough)
+        {
+            return;
+        }
+
+        float signedHeat = Mathf.Sign(diff) * Props.heatPerSecond;
+
+        GenTemperature.PushHeat(parent.Position, map, signedHeat);
+    }
+
+    private float GetDesiredTargetTemperature(float currentTemp)
+    {
+        FloatRange range = Props.targetTemperatureRange;
+
+        float distMin = Mathf.Abs(currentTemp - range.min);
+        float distMax = Mathf.Abs(currentTemp - range.max);
+
+        return distMin <= distMax ? range.min : range.max;
+    }
+
+    public override string CompInspectStringExtra()
+    {
+        // TODO
+        return null;
+    }
+}
---
+++ Source/TheSecretOfAnimaCore/Comps/CompProperties_BiHeatPusher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace tsoa.core;
+
+public class CompProperties_BiHeatPusher : CompProperties
+{
+    public FloatRange targetTemperatureRange;
+
+    public float heatPerSecond = 10f;
+
+    public bool worksOutdoors = false;
+
+    public CompProperties_BiHeatPusher()
+    {
+        compClass = typeof(CompBiHeatPusher);
+    }
+}
